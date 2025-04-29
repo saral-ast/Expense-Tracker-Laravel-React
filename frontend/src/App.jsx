11084192f4a62389379a9f742e8419e70504b47a
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-04-29 14:47:13 +0530

    chore: initialize project structure with backend and frontend setup
    This commit introduces the initial project structure, including backend and frontend configurations. The backend is set up with Laravel, including routes, controllers, models, migrations, and database configurations. The frontend is configured with React, Vite, and TailwindCSS. Essential files such as `.gitignore`, `package.json`, and configuration files for both backend and frontend are added to ensure a smooth development environment.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1 className='text-3xl'>Hello World</h1>
+          </>
+  )
+}
+
+export default App
